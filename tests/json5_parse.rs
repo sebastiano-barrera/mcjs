b88d7dfaa79efb179fc6cdf9f35ae481742e9f13
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2023-02-04 11:53:53 +0100

    add integration test based on `json5` npm package

---
+++ tests/json5_parse.rs
@@ -0,0 +1,15 @@
+extern crate mcjs;
+
+use std::path::PathBuf;
+
+#[test]
+fn test_load_json5_parse() {
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let include_path = manifest_dir.join("test-resources/modules/json5/lib");
+
+    let mut vm = mcjs::VM::new();
+    vm.add_include_path(include_path.to_owned()).unwrap();
+    vm.run_script("require('parse')".to_string(), Default::default()).unwrap();
+
+    assert!(false);
+}
