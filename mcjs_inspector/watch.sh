a8036823a7e65578a31325073323a511564b9ddc
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2023-10-18 22:16:24 +0200

    remove mcjs_inspector (to be reintroduced later)

--- mcjs_inspector/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export RUST_LOG=debug
-
-filename="${1:-/tmp/mcjs-inspector-0.case}"
-echo "case filename: $filename"
-
-systemfd --no-pid -s http::10001 -- cargo watch -s "cargo run --bin mcjs_inspector ${filename}"
-
