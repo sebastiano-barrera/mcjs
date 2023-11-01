acfd6acbad77dfaad1d14d5912736c8abfd323f2
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2023-11-01 01:39:41 +0100

    mcjs_tools: cooler monospace fonts

--- mcjs_tools/tailwind.config.js
+++ mcjs_tools/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./templates/**/*.html", "./templates/**/*.ejs"],
   theme: {
     extend: {},
+    fontFamily: {
+      'mono': ['Iosevka Extended', 'Source Code Pro', 'ui-monospace']
+    }
   },
   plugins: [],
 }
+
