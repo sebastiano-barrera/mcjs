720645c9a9197026f6f90e66917a0a42fb19732c
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2023-11-07 00:31:56 +0100

    mcjs_tools: "Source" pane (dummy content)

--- mcjs_tools/src/main.rs
+++ mcjs_tools/src/main.rs
@@ -61,6 +61,7 @@ async fn main() -> Result<()> {
             .service(actix_files::Files::new("/assets", "./data/assets/"))
             .service(main_screen)
             .service(events)
+            .service(some_text)
             .app_data(data_ref.clone())
     });
 
@@ -142,6 +143,26 @@ async fn events(app_data: web::Data<AppData<'static>>) -> impl Responder {
     sse::Sse::from_infallible_receiver(rx).with_retry_duration(Duration::from_secs(10))
 }
 
+#[actix_web::get("/some_text")]
+async fn some_text() -> impl Responder {
+    HttpResponse::Ok().body(
+        "
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit
+esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+voluptas nulla pariatur?
+        ",
+    )
+}
+
 mod interpreter_manager {
     use mcjs_vm::interpreter::debugger::Probe;
 
@@ -206,10 +227,7 @@ mod interpreter_manager {
         Resume,
     }
 
-    fn interpreter_main_loop(
-        main_path: PathBuf,
-        queue_rx: mpsc::Receiver<Message>,
-    ) -> Result<()> {
+    fn interpreter_main_loop(main_path: PathBuf, queue_rx: mpsc::Receiver<Message>) -> Result<()> {
         use mcjs_vm::interpreter::Exit;
         use mcjs_vm::{Interpreter, Loader, Realm};
 
@@ -276,7 +294,6 @@ mod interpreter_manager {
                     println!("(resuming)");
                 }
                 Err(mut err) => {
-
                     let error = {
                         let mut message = String::new();
                         for msg in err.error.messages() {
