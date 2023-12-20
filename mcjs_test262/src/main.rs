caf2a43cd6578eb64579cf5442ab128d30a3ba4d
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2023-12-20 18:14:18 +0100

    mcjs_test262: simplify/fix test runner

--- mcjs_test262/src/main.rs
+++ mcjs_test262/src/main.rs
@@ -1,49 +1,4 @@
-use anyhow::Result;
 use std::path::Path;
-
-struct TestEnv<'a> {
-    /// Absolute path to the test262 root folder.
-    test262_root: &'a Path,
-}
-
-struct TestParams<'a> {
-    env: &'a TestEnv<'a>,
-
-    /// Path to the test file.  Must be relative.  It will be interpreted
-    /// relative to the test262 root folder.
-    filename: &'a Path,
-}
-
-fn run_test262<'a>(params: &TestParams<'a>) -> Result<()> {
-    let mut loader = mcjs_vm::Loader::new(None);
-
-    let chunk_paths = [
-        Path::new("harness/assert.js"),
-        Path::new("harness/sta.js"),
-        params.filename,
-    ];
-
-    let mut chunk_fnids = Vec::with_capacity(chunk_paths.len());
-    for file_path in chunk_paths {
-        let content = std::fs::read_to_string(file_path)?;
-
-        let file_path_str = file_path.to_string_lossy().into_owned();
-        let chunk_fnid: mcjs_vm::bytecode::FnId = loader
-            .load_script(Some(file_path_str), content)
-            .map_err(|err| anyhow::anyhow!("while loading script: {:?}", err))?;
-
-        chunk_fnids.push(chunk_fnid);
-    }
-
-    for fnid in chunk_fnids {
-        let mut realm = mcjs_vm::Realm::new();
-        mcjs_vm::Interpreter::new(&mut realm, &mut loader, fnid)
-            .run()
-            .map_err(|err| anyhow::anyhow!("error while running code: {:?}", err))?;
-    }
-
-    Ok(())
-}
 
 fn main() {
     let test262_root = match std::env::var("TEST262_ROOT") {
@@ -56,13 +11,35 @@ fn main() {
         }
     };
 
-    let test_env = TestEnv {
-        test262_root: &test262_root,
-    };
-    let test_params = TestParams {
-        env: &test_env,
-        filename: Path::new("test/language/function-code/10.4.3-1-1-s.js"),
-    };
+    let filename = Path::new("test/language/function-code/10.4.3-1-1-s.js");
 
-    run_test262(&test_params).expect("test failed");
+    let mut loader = mcjs_vm::Loader::new(None);
+
+    let chunk_paths = [
+        test262_root.join(Path::new("harness/assert.js")),
+        test262_root.join(Path::new("harness/sta.js")),
+        filename.to_path_buf(),
+    ];
+
+    let mut chunk_fnids = Vec::with_capacity(chunk_paths.len());
+    for file_path in chunk_paths {
+        let file_path_str = file_path.to_string_lossy().into_owned();
+        eprintln!(" (..) reading {}", file_path_str);
+        let content = std::fs::read_to_string(file_path).expect("read_to_string");
+
+        eprintln!(" (..) loading {}", file_path_str);
+        let chunk_fnid: mcjs_vm::bytecode::FnId =
+            loader.load_script(Some(file_path_str), content).unwrap();
+
+        chunk_fnids.push(chunk_fnid);
+    }
+
+    let mut realm = mcjs_vm::Realm::new();
+
+    for fnid in chunk_fnids {
+        eprintln!(" (..) running {:?}", fnid);
+        mcjs_vm::Interpreter::new(&mut realm, &mut loader, fnid)
+            .run()
+            .expect("run");
+    }
 }
