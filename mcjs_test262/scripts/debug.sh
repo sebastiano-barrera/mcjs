ae897bfb70208fb6c8bf3a18870bbe878c41a573
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2023-12-29 16:22:06 +0100

    mcjs_test262: simpler debug.sh, allow absolute paths

--- mcjs_test262/scripts/debug.sh
+++ mcjs_test262/scripts/debug.sh
@@ -2,28 +2,22 @@
 
 set -e
 
+TEST262_ROOT="$(realpath "${TEST262_ROOT:-$HOME/src/test262/}")"
+
+here="$(dirname "$0")"
+DBG_ROOT="${here}/../../mcjs_tools/"
+cd "$DBG_ROOT"
+
 if [[ "$#" -lt 1 ]]
 then
 	echo "usage: $0 <path to test262 test case.js>"
 	exit 1
 fi
 
-here="$(dirname "$0")"
-
-TEST262_ROOT="$(realpath "${TEST262_ROOT:-$HOME/src/test262/}")"
-DBG_ROOT="${here}/../../mcjs_tools/"
-cd "$DBG_ROOT"
-
-test_case="$(realpath "$TEST262_ROOT/$1")"
-if [ ! -f "$test_case" ]
-then
-	echo "no such file: $test_case"
-	exit 1
-fi
+test_case="$1"
 
 cargo run -- \
 	"$TEST262_ROOT/harness/assert.js" \
 	"$TEST262_ROOT/harness/sta.js" \
 	"$test_case"
-
 
