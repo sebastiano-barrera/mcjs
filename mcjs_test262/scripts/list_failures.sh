37313e96df0a7dd9332aa07c28d1584fa333f4b4
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-01-12 22:36:57 +0100

    mcjs_test262: add support for '-dirty' suffix to more scripts

--- mcjs_test262/scripts/list_failures.sh
+++ mcjs_test262/scripts/list_failures.sh
@@ -3,7 +3,9 @@
 here="$(dirname "$0")"
 cd "$here"
 
-version="$(git rev-parse HEAD)"
+suffix=""
+[ -z "$(git status --porcelain)" ] || suffix="-dirty"
+version="$(git rev-parse HEAD)$suffix"
 
 sqlite3 ../out/tests.db "select path from general where not success and version like '${version}%'"
 
