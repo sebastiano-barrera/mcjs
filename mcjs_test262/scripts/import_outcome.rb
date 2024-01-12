68b1fb3729be80a16aae48c053c307ba1fda0c99
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-01-12 21:48:08 +0100

    mcjs_test262: track mcjs version in tests.db

--- mcjs_test262/scripts/import_outcome.rb
+++ mcjs_test262/scripts/import_outcome.rb
@@ -22,6 +22,8 @@ end
 
 Dir.chdir __dir__
 
+mcjs_version = Git::CommitID.get_current.to_s
+
 filename = '../out/runs.json'
 STDERR.puts "loading #{filename}..."
 runs = File.new(filename).each_line.map{|line| JSON::load line}
@@ -29,15 +31,15 @@ runs = File.new(filename).each_line.map{|line| JSON::load line}
 STDERR.puts "inserting into db..."
 db = Database.new '../out/tests.db'
 db.transaction do
-  db.delete_all_runs
+  db.delete_runs_for_version(mcjs_version)
   runs.each do |run|
     record = {
       :path => run["file_path"],
       :is_strict => run["is_strict"] ? 1 : 0,
       :error_category => run.deep_get("error", "category"),
       :error_message => run.deep_get("error", "message"),
+      :version => mcjs_version,
     }
-    puts record
     db.insert_run record
   end
 end
