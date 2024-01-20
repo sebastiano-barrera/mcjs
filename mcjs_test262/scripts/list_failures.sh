7dc410fe1c7e65a39b5f86c631c18d7d160dd44b
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-01-20 13:35:27 +0100

    mcjs_test262: more scripts use the simple 'dirty' tag

--- mcjs_test262/scripts/list_failures.sh
+++ mcjs_test262/scripts/list_failures.sh
@@ -3,9 +3,8 @@
 here="$(dirname "$0")"
 cd "$here"
 
-suffix=""
-[ -z "$(git status --porcelain)" ] || suffix="-dirty"
-version="$(git rev-parse HEAD)$suffix"
+version="$(git rev-parse HEAD)"
+[ -z "$(git status --porcelain)" ] || version="dirty"
 
 sqlite3 ../out/tests.db "select path from general where not success and version = '${version}'"
 
