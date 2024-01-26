1c44dfb06ccbdda01e774ee54fce6a19202a6831
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-01-26 23:33:13 +0100

    tracing: fix multithreaded usage

--- mcjs_vm/src/tracing.rs
+++ mcjs_vm/src/tracing.rs
@@ -1,21 +1,17 @@
 use lazy_static::lazy_static;
 
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::{Result, Write};
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex, MutexGuard};
+use std::thread_local;
 
 pub struct Config {
     pub sink: SinkType,
 }
-
-pub enum SinkType {
-    File(PathBuf),
-    Stdout,
-    Buffer,
-}
-
 impl Default for Config {
     fn default() -> Self {
         Config {
@@ -24,77 +20,121 @@ impl Default for Config {
     }
 }
 
+pub enum SinkType {
+    File(PathBuf),
+    Stdout,
+    Buffer,
+}
+
 pub fn init(config: Config) {
-    let mut logger = get();
-    *logger = Logger::new(config);
-}
-
-#[must_use]
-pub fn section(header: &str) -> Section {
-    let mut logger = get();
-    logger.start_section(header);
-    Section
-}
-
-pub struct Section;
-impl Section {
-    pub fn log_value<T: Debug>(&self, tag: &str, value: &T) {
-        let mut logger = get();
-        logger.log_value(tag, value);
-    }
-
-    pub fn log(&self, tag: &str, value: &str) {
-        let mut logger = get();
-        logger.log(tag, value);
-    }
-}
-
-impl Drop for Section {
-    fn drop(&mut self) {
-        let mut logger = get();
-        logger.end_section();
-    }
+    let mut sink = get_sink();
+    *sink = match config.sink {
+        SinkType::File(filename) => {
+            let file = File::open(filename).expect("could not open logger sink file");
+            Sink::File(file)
+        }
+        SinkType::Stdout => Sink::Stdout,
+        SinkType::Buffer => Sink::Buffer(String::new()),
+    };
 }
 
 lazy_static! {
-    static ref GLOBAL_LOGGER: Arc<Mutex<Logger>> = Arc::new(Mutex::new(Logger::default()));
+    static ref GLOBAL_SINK: Arc<Mutex<Sink>> = Arc::new(Mutex::new(Sink::Stdout));
 }
 
-fn get() -> MutexGuard<'static, Logger> {
-    GLOBAL_LOGGER.lock().expect("could not lock global logger!")
+fn get_sink() -> MutexGuard<'static, Sink> {
+    GLOBAL_SINK.lock().expect("could not lock global logger!")
 }
 
-struct Logger {
-    sink: Sink,
-    sections_depth: usize,
-}
 enum Sink {
     File(File),
     Stdout,
     Buffer(String),
 }
+impl Sink {
+    fn write(&mut self, data: String) {
+        match self {
+            Sink::File(f) => {
+                f.write(data.as_bytes()).unwrap();
+            }
+            Sink::Stdout => {
+                let mut stdout = std::io::stdout().lock();
+                stdout.write(data.as_bytes()).unwrap();
+            }
+            Sink::Buffer(buf) => {
+                buf.push_str(&data);
+            }
+        }
+    }
+
+    #[cfg(test)]
+    fn take_buffer(&mut self) -> String {
+        let buf = match self {
+            Sink::Buffer(buf) => buf,
+            _ => panic!("take_buffer, but sink is not Sink::Buffer(_)"),
+        };
+
+        std::mem::replace(buf, String::new())
+    }
+}
+
+thread_local! {
+    static THREAD_LOGGER: RefCell<Logger> = RefCell::new(Logger::new());
+}
+
+#[must_use]
+pub fn section(header: &str) -> Section {
+    THREAD_LOGGER.with(|logger| {
+        let mut logger = logger.borrow_mut();
+        logger.start_section(header);
+
+        Section
+    })
+}
+
+pub struct Section;
+impl Section {
+    pub fn log_value<T: Debug>(&self, tag: &str, value: &T) {
+        THREAD_LOGGER.with(|logger| {
+            let mut logger = logger.borrow_mut();
+            logger.log_value(tag, value);
+        })
+    }
+
+    pub fn log(&self, tag: &str, value: &str) {
+        THREAD_LOGGER.with(|logger| {
+            let mut logger = logger.borrow_mut();
+            logger.log(tag, value);
+        })
+    }
+}
+
+impl Drop for Section {
+    fn drop(&mut self) {
+        THREAD_LOGGER.with(|logger| {
+            let mut logger = logger.borrow_mut();
+            logger.end_section();
+        })
+    }
+}
+
+struct Logger {
+    buf: String,
+    sections_depth: usize,
+}
 
 impl Default for Logger {
     fn default() -> Self {
-        Logger::new(Config::default())
+        Logger {
+            buf: String::new(),
+            sections_depth: 0,
+        }
     }
 }
 
 impl Logger {
-    fn new(config: Config) -> Self {
-        let sink = match config.sink {
-            SinkType::File(filename) => {
-                let file = File::open(filename).expect("could not open tracing sink file");
-                Sink::File(file)
-            }
-            SinkType::Stdout => Sink::Stdout,
-            SinkType::Buffer => Sink::Buffer(String::new()),
-        };
-
-        Logger {
-            sink,
-            sections_depth: 0,
-        }
+    fn new() -> Self {
+        Logger::default()
     }
 
     fn start_section(&mut self, header: &str) {
@@ -106,30 +146,22 @@ impl Logger {
         assert!(self.sections_depth > 0);
         self.sections_depth -= 1;
         self.write("</>");
+
+        if self.sections_depth == 0 {
+            let mut sink = get_sink();
+            let buf = std::mem::replace(&mut self.buf, String::new());
+            sink.write(buf);
+        }
     }
 
     fn write(&mut self, text: &str) {
         use std::fmt::Write;
 
-        let mut line_buf = String::new();
         for line in text.split('\n') {
             for _ in 0..self.sections_depth {
-                write!(line_buf, "    ").unwrap();
+                write!(self.buf, "    ").unwrap();
             }
-            writeln!(line_buf, "{}", line).unwrap();
-        }
-
-        match &mut self.sink {
-            Sink::File(f) => {
-                f.write(line_buf.as_bytes()).unwrap();
-            }
-            Sink::Stdout => {
-                let mut stdout = std::io::stdout().lock();
-                stdout.write(line_buf.as_bytes()).unwrap();
-            }
-            Sink::Buffer(buf) => {
-                buf.push_str(&line_buf);
-            }
+            writeln!(self.buf, "{}", line).unwrap();
         }
     }
 
@@ -138,15 +170,6 @@ impl Logger {
     }
     fn log(&mut self, tag: &str, value: &str) {
         self.write(&format!("{}: {}\n", tag, value));
-    }
-
-    fn take_buffer(&mut self) -> String {
-        let buf = match &mut self.sink {
-            Sink::Buffer(buf) => buf,
-            _ => panic!("take_buffer, but sink is not Sink::Buffer(_)"),
-        };
-
-        std::mem::replace(buf, String::new())
     }
 }
 
@@ -165,7 +188,7 @@ mod tests {
             _s.log_value("the_value", &(1, 2, 3));
         }
 
-        let output = get().take_buffer();
+        let output = get_sink().take_buffer();
         insta::assert_snapshot!(output);
     }
 }
