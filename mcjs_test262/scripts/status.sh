b1d52be494431e689435321dff180e1f38d1dd69
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-02-08 21:47:10 +0100

    mcjs_test262: status.sh: use long-form query directly

--- mcjs_test262/scripts/status.sh
+++ mcjs_test262/scripts/status.sh
@@ -5,5 +5,29 @@ cd "$(dirname "$0")"
 version="$(git rev-parse HEAD)"
 [ -z "$(git status --porcelain)" ] || version="dirty"
 
-sqlite3 -table ../out/tests.db "select * from status where version like '${version}%'" >../out/status.txt
+{
+	echo "# version: $version"
+	sqlite3 -box ../out/tests.db <<-EOF 
+	with q as (
+		select dirname
+		, success
+		, count(*) as count
+		from general
+		where version = '${version}'
+		group by dirname, success 
+		order by dirname, success
+	)
+	, q2 as (
+		select dirname
+		, ifnull(sum(count) filter (where success = 1), 0) as ok
+		, ifnull(sum(count) filter (where success = 0), 0) as fail
+		from q
+		group by dirname
+	)
+	select *
+	, printf('%6.2f', cast(ok as real) * 100 / (ok + fail)) as progress
+	from q2
+	;
+EOF
+} | tee ../out/status.txt
 
