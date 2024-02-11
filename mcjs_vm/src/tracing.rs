2f82fb451f228cea15dc155dd2b72ae80e78308a
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-02-11 17:18:46 +0100

    tracing: simplify

--- mcjs_vm/src/tracing.rs
+++ mcjs_vm/src/tracing.rs
@@ -32,6 +32,11 @@ mod internal {
     use std::sync::{Arc, Mutex, MutexGuard};
     use std::thread_local;
 
+    //
+    // Public API
+    // (does not require a handle, simply forwards into *the* thread-local handle)
+    //
+
     #[must_use]
     pub fn section(header: &str) -> Section {
         #[cfg(test)]
@@ -60,13 +65,6 @@ mod internal {
                 logger.log(tag, value);
             })
         }
-
-        pub fn copy_buffer(&self) -> String {
-            THREAD_LOGGER.with(|logger| {
-                let logger = logger.borrow_mut();
-                logger.buffer().to_string()
-            })
-        }
     }
 
     impl Drop for Section {
@@ -79,94 +77,43 @@ mod internal {
     }
 
     struct SinkConfig {
-        sink: SinkType,
+        sink_filename: PathBuf,
     }
     impl Default for SinkConfig {
         fn default() -> Self {
             SinkConfig {
-                sink: SinkType::Stdout,
+                sink_filename: PathBuf::from("/dev/stdout"),
             }
         }
     }
     impl SinkConfig {
         fn from_env() -> Self {
-            let sink = match std::env::var("MCJS_TRACING_SINK") {
-                Ok(value) if value == "@discard" => SinkType::Discard,
-                Ok(filename) => SinkType::File(PathBuf::from(filename)),
-                _ => SinkType::Stdout,
-            };
-            SinkConfig { sink }
+            let sink_filename: PathBuf = std::env::var("MCJS_TRACING_SINK")
+                .unwrap_or_else(|_| "/dev/stdout".to_string())
+                .into();
+            SinkConfig { sink_filename }
         }
     }
 
-    enum SinkType {
-        File(PathBuf),
-        Stdout,
-        Buffer,
-        Discard,
-    }
-
-    lazy_static! {
-        static ref GLOBAL_SINK: Arc<Mutex<Sink>> =
-            Arc::new(Mutex::new(Sink::from_config(SinkConfig::from_env())));
-    }
-
-    fn get_sink() -> MutexGuard<'static, Sink> {
-        GLOBAL_SINK.lock().expect("could not lock global logger!")
-    }
-    fn set_sink(new_sink: Sink) {
-        let mut sink = get_sink();
-        *sink = new_sink;
-    }
-
-    enum Sink {
-        File(File),
-        Stdout,
-        Buffer(String),
-        Discard,
-    }
-    impl Sink {
-        fn from_config(config: SinkConfig) -> Self {
-            match config.sink {
-                SinkType::File(filename) => {
-                    let file = File::create(filename).expect("could not open logger sink file");
-                    Sink::File(file)
-                }
-                SinkType::Stdout => Sink::Stdout,
-                SinkType::Buffer => Sink::Buffer(String::new()),
-                SinkType::Discard => Sink::Discard,
-            }
-        }
-
-        fn write(&mut self, data: &str) {
-            match self {
-                Sink::File(f) => {
-                    f.write(data.as_bytes()).unwrap();
-                }
-                Sink::Stdout => {
-                    let mut stdout = std::io::stdout().lock();
-                    stdout.write(data.as_bytes()).unwrap();
-                }
-                Sink::Buffer(buf) => {
-                    buf.push_str(data);
-                }
-                Sink::Discard => {}
-            }
-        }
-    }
+    //
+    // Thread-local part
+    //
 
     thread_local! {
         static THREAD_LOGGER: RefCell<Logger> = RefCell::new(Logger::new());
     }
 
     struct Logger {
+        thread_prefix: String,
         buf: String,
         sections_depth: usize,
     }
 
     impl Default for Logger {
         fn default() -> Self {
+            let thread_id = format!("{:?}: ", std::thread::current().id());
             Logger {
+                thread_prefix: thread_id,
                 buf: String::new(),
                 sections_depth: 0,
             }
@@ -179,43 +126,106 @@ mod internal {
         }
 
         fn start_section(&mut self, header: &str) {
-            self.write(&format!("<{}>", header));
+            self.writeln(&format!("<{}>", header));
             self.sections_depth += 1;
+        }
+
+        fn log_value<T: Debug>(&mut self, tag: &str, value: &T) {
+            self.writeln(&format!("{}: {:#?}", tag, value));
+        }
+        fn log(&mut self, tag: &str, value: &str) {
+            self.writeln(&format!("{}: {}", tag, value));
         }
 
         fn end_section(&mut self) {
             assert!(self.sections_depth > 0);
             self.sections_depth -= 1;
-            self.write("</>");
-
-            if self.sections_depth == 0 {
-                let mut sink = get_sink();
-                sink.write(&self.buf);
-                self.buf.clear();
-            }
+            self.writeln("</>");
         }
 
-        fn write(&mut self, text: &str) {
+        fn writeln(&mut self, text: &str) {
             use std::fmt::Write;
 
+            self.buf.clear();
             for line in text.split('\n') {
+                self.buf.push_str(&self.thread_prefix);
                 for _ in 0..self.sections_depth {
                     write!(self.buf, "    ").unwrap();
                 }
-                writeln!(self.buf, "{}", line).unwrap();
+
+                write!(self.buf, "{}\n", line).unwrap();
+            }
+
+            // flush global sink file. inefficient, but tracing is often employed to debug VM
+            // crashes. we might be crashing right after returning, so let's work to make it more
+            // likely that the data is not lost.
+            let mut sink = get_global_sink();
+            sink.write(&self.buf);
+        }
+    }
+
+    //
+    // Global part (process-wide, thread-nonlocal)
+    //
+    lazy_static! {
+        static ref GLOBAL_SINK: Arc<Mutex<Sink>> =
+            Arc::new(Mutex::new(Sink::from_config(SinkConfig::from_env())));
+    }
+
+    fn get_global_sink() -> MutexGuard<'static, Sink> {
+        GLOBAL_SINK.lock().expect("could not lock global logger!")
+    }
+
+    #[cfg(test)]
+    fn set_global_sink_memory() {
+        let mut gsink = GLOBAL_SINK.lock().expect("could not lock global logger!");
+        *gsink = Sink::Memory(String::new());
+    }
+
+    #[cfg(test)]
+    fn read_memory_buffer_this_thread() -> String {
+        let mut gsink = GLOBAL_SINK.lock().expect("could not lock global logger!");
+        let buf = match &mut *gsink {
+            Sink::Memory(buf) => buf,
+            _ => panic!("global sink not initialized as memory buffer"),
+        };
+
+        let prefix = format!("{:?}: ", std::thread::current().id());
+        let mut tl_buf = String::new();
+        for line in buf.lines() {
+            if let Some(tl_line) = line.strip_prefix(&prefix) {
+                tl_buf.push_str(tl_line);
+                tl_buf.push('\n');
             }
         }
 
-        fn log_value<T: Debug>(&mut self, tag: &str, value: &T) {
-            self.write(&format!("{}: {:#?}\n", tag, value));
-        }
-        fn log(&mut self, tag: &str, value: &str) {
-            self.write(&format!("{}: {}\n", tag, value));
+        tl_buf
+    }
+
+    enum Sink {
+        File(File),
+        Memory(String),
+    }
+    impl Sink {
+        fn from_config(config: SinkConfig) -> Self {
+            let file = File::create(config.sink_filename).expect("could not open logger sink file");
+            Sink::File(file)
         }
 
-        #[cfg(test)]
-        fn buffer(&self) -> &str {
-            &self.buf
+        fn write(&mut self, data: &str) {
+            match self {
+                Sink::File(f) => {
+                    f.write(data.as_bytes()).unwrap();
+
+                    // inefficient, but tracing is often employed when the program is crashing. we might be
+                    // crashing right after returning from this call, so let's flush immediately.
+                    f.flush().unwrap();
+                }
+                Sink::Memory(buf) => {
+                    // no flushing necessary
+                    buf.push_str(data);
+                }
+            }
         }
     }
 
@@ -225,11 +235,15 @@ mod internal {
 
         #[test]
         fn test_section() {
-            let output = {
+            set_global_sink_memory();
+
+            {
                 let _s = section("section name");
                 _s.log_value("the_value", &(1, 2, 3));
-                _s.copy_buffer()
-            };
+            }
+
+            let output = read_memory_buffer_this_thread();
+            // Remove thread ID
             insta::assert_snapshot!(output);
         }
     }
